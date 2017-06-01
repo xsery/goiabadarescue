38f24f2fdb298830554eae9cd5090ae5941f978c
linkhenrique <link.jogosdigitais@gmail.com>
2017-06-01 15:11:12 -0300

    Alteração

--- QueijoGoiaba/js/game.js
+++ QueijoGoiaba/js/game.js
@@ -29,7 +29,7 @@ GameState.prototype.preload = function() {
     this.game.load.audio('pickupSound', 'Assets/sounds/inimigodead.wav');
     this.game.load.audio('playerDeath', 'Assets/sounds/morte.ogg');
     this.game.load.audio('enemyDeath', 'Assets/sounds/inimigodead.ogg');
-    this.game.load.audio('music', 'Assets/sounds/salgado.ogg');
+    this.game.load.audio('music', 'Assets/sounds/doce.ogg');
 }
 
 GameState.prototype.create = function() { 
@@ -50,6 +50,7 @@ GameState.prototype.create = function() {
     // A ordem nesse caso é importante, então os layers que ficarão no "fundo" deverão ser
     // criados primeiro, e os que ficarão na "frente" por último;
     this.bgLayer = this.level1.createLayer('BG');
+    this.lavaLayer = this.level1.createLayer('Itens');
     this.lavaLayer = this.level1.createLayer('Lava');
     this.wallsLayer = this.level1.createLayer('Walls');
     
@@ -67,11 +68,11 @@ GameState.prototype.create = function() {
     // devem colidir, pois há mais tiles que colidem do que tiles sem colisão.
     // Os parâmetros são a lista dos tiles, "true" indicando que a colisão deve ser ativada,
     // e o nome do layer.
-    this.level1.setCollisionByExclusion([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 352], true, this.wallsLayer);
+    this.level1.setCollisionByExclusion([2, 22, 173, 174, 326, 481, 632, 633, 785, 786, 938, 1091, 1244, 1397, 1550, 1703, 1856, 2009, 2162, 2315],  true, this.wallsLayer);
     
     // Para o layer de lava é o caso oposto: poucos tiles colidem, então é mais fácil 
     // informar diretamente quais são.
-    this.level1.setCollision([88], true, this.lavaLayer);
+    this.level1.setCollision([481], true, this.lavaLayer);
     
         
     // Inicializando jogador
