3b01aa938ba662c39d71c4336505c0d9ddc657ff
Samuel Queiroz Silva <samqueiroz90@gmail.com>
2017-05-31 21:44:14 -0400

    Adicionar musica de menu
    Adicionando musica de menu

--- QueijoGoiaba/js/menu.js
+++ QueijoGoiaba/js/menu.js
@@ -5,25 +5,30 @@ MenuState.prototype.preload = function(){
     
     this.game.load.image('newGame', 'Assets/buttons/player.png');
     this.game.load.image('credits', 'Assets/buttons/player.png');
+    this.game.load.audio('musicMenu', 'Assets/sounds/menu.ogg');
     
 }
 
 MenuState.prototype.create = function(){
     
-    this.newGame = game.add.sprite(100, 100, 'newGame');
+    this.newGame = game.add.sprite(150, 300, 'newGame');
     this.newGame.anchor.set(0.5);
     this.newGame.inputEnabled = true;
     this.newGame.events.onInputDown.add(this.listener, this);
     
-    this.credits = game.add.sprite(100, 300, 'credits');
+    this.credits = game.add.sprite(400, 300, 'credits');
     this.credits.anchor.set(0.5);
     this.credits.inputEnabled = true;
     this.credits.events.onInputDown.add(this.listenerCredits, this);
+    
+    this.musicMenu = game.add.audio('musicMenu');
+    this.musicMenu.play();
     
 }
 
 
 MenuState.prototype.update = function(){
+    
     
 }
 
